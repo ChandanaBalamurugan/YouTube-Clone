52df8dbad60e5e345281813eda1cc26bd2f8cc15
ChandanaBalamurugan <chandanabalamurugan05@gmail.com>
2026-04-14 10:34:51 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const menuButton=document.querySelectorAll(".menu-button");
+const screenOverlay=document.querySelector(".screen-overlay");
+const themeButton=document.querySelector(".theme-button i");
+
+//initialize darl mode based on localstorage
+if(localStorage.getItem("darkMode") === "enabled") {
+  document.body.classList.add("dark-mode");
+  themeButton.classList.replace("uil-moon","uil-sun");
+}else{
+    themeButton.classList.replace("uil-sun","uil-moon");
+}
+
+//toggle dark mode when theme button is clicked
+themeButton.addEventListener("click",()=>{
+    const isDarkMode = document.body.classList.toggle("dark-mode");
+    localStorage.setItem("darkMode",isDarkMode ? "enabled" : "disabled");
+    themeButton.classList.toggle("uil-sun",isDarkMode);
+    themeButton.classList.toggle("uil-moon",!isDarkMode);
+});
+
+//toggle sidebar visibility when menu buttons are clicked
+menuButton.forEach(button => {
+    button.addEventListener("click",()=>{
+    document.body.classList.toggle("sidebar-hidden");
+});
+});
+
+screenOverlay.addEventListener("click", ()=>{
+    document.body.classList.toggle("sidebar-hidden");
+});
+
+if(window.innerWidth >=768) {
+    document.body.classList.remove("sidebar-hidden");
+}
